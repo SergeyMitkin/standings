003c6bc8896852bd1148a6e1ff6d0435c1220a7f
SergeyMitkin <sergeymitkin@gmail.com>
2021-09-05 06:53:33 +0300

    Создан crud для teams

---
+++ backend/models/tables/Teams.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "teams".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class Teams extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'teams';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 250],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+}
